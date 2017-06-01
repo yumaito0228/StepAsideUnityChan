59a774a84ce1dfb0fd197283b5aa0c306d75a8f9
yumaito <yuma19950228@icloud.com>
2017-06-01 15:45:20 +0900

    不要になったアイテムを順次破棄&ユニティちゃんの位置に応じてアイテムを動的に生成 《条件》 StepAsideUnityChanをもとに作成 ユニティちゃんが通り過ぎて画面外に出たアイテムを直ちに破棄 アイテムの破棄にはDestroy関数を使用 ユニティちゃんの進行に応じてアイテムを動的に生成 Unityちゃんから見て常に40〜50m程度先までアイテムが生成されている状態

---
+++ Assets/Lesson6/detectcontroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectcontroller : MonoBehaviour {
+
+	public GameObject Unitychan;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = this.transform.position;
+		pos.z = Unitychan.transform.position.z - 8;
+		this.transform.position = pos;
+	}
+	void OnTriggerEnter(Collider other) {
+		//障害物に衝突した場合
+		if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag") {
+			Destroy (other.gameObject);
+		}
+		//コインに衝突した場合
+		if (other.gameObject.tag == "CoinTag") {
+			//接触したコインのオブジェクトを破棄
+			Destroy (other.gameObject);
+		}
+	}
+}
